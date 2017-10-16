52c1c840158b289397f2a6a96ec3714aed1a6622
windatm <tuanphongmta0205@gmail.com>
2017-10-16 14:16:26 +0700

    Update

--- ql kho hang/QuanLyKho/QuanLyKho/frmHuongDan.cs
+++ ql kho hang/QuanLyKho/QuanLyKho/frmHuongDan.cs
@@ -13,6 +13,7 @@ namespace QuanLyKho
     public partial class frmHuongDan : Form
     {
 
+
         public frmHuongDan()
         {
             InitializeComponent();
@@ -28,6 +29,18 @@ namespace QuanLyKho
             {
                 txtHD.Text = "Mọi thông tin hay thắc mắc về phần mềm liên hệ \r\nSĐT: 0987890957 \r\nEmail: dao1812@gmail.com";
             }
+            else if (treeView1.SelectedNode.Name == "nDangNhap")
+            {
+                txtHD.Text = "Để sử dụng phần mềm bạn cần có tài khoản. Bạn có thể liên hệ với tác giả trong phần thông tin liên hệ để đăng kí tài khoản";
+            }
+            else if (treeView1.SelectedNode.Name == "nNhapHang")
+            {
+                txtHD.Text = @"Để nhập thêm hàng hóa, bạn click vào ô 'Nhập hàng', sau đó điền đầy đủ thông tin và click vào ô 'Nhập thêm' hoặc 'Nhập mới";
+            }
+            else if (treeView1.SelectedNode.Name == "nXuatHang")
+            {
+                txtHD.Text = @"Để xuất hàng hóa, bạn chọn nội dung cần xuất trong danh sách ở dưới, sau đó sửa lại thông tin và click vào ô 'Xuất' để hoàn tất";
+            }
             else if (treeView1.SelectedNode.Name == "nTimKiem")
             {
                 txtHD.Text = @"Để tìm kiếm nội dung nào đó, bạn nhập thông tin vào ô trống trong phần tìm kiếm sau đó click vào nội dung mình cần tìm kiếm";
@@ -37,6 +50,7 @@ namespace QuanLyKho
                 txtHD.Text = @"Để thống kê , bạn nhập click vào ô ' Thống kê nhập' hoặc 'Thống kê xuất";
             }
         }
+
 
 
     }

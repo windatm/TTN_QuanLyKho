59470e7147d2700e7780c244a26c9ff6513a269a
MTAZero <buithuy3996@gmail.com>
2017-10-16 16:01:22 +0700

    FrmNhapHang

--- ql kho hang/QuanLyKho/QuanLyKho/frmNhapHang.cs
+++ ql kho hang/QuanLyKho/QuanLyKho/frmNhapHang.cs
@@ -18,6 +18,38 @@ namespace QuanLyKho
             InitializeComponent();
         }
 
+        ConnectData conn = new ConnectData();
+        public string constr = @"select * from tblHangHoa";
+        public string consql = @"Data Source=UBUNTU;Initial Catalog=Project_4;Integrated Security=True";
+
+        private void frmNhapHang_Load(object sender, EventArgs e)
+        {
+            conn.KhoiTao(dgv_Hanghoa, constr);
+        }
+
+        private void butIns_Click(object sender, EventArgs e)
+        {
+            conn.MoKetNoi();
+            SqlCommand sqlcm = new SqlCommand("NhapMoi", conn.conn);
+            sqlcm.CommandType = CommandType.StoredProcedure;
+            sqlcm.Parameters.AddWithValue("@MaHH", txtMaHH.Text.Trim());
+            sqlcm.Parameters.AddWithValue("@TenHH", txtTenHH.Text.Trim());
+            sqlcm.Parameters.AddWithValue("@MaNCC", txtMaNCC.Text.Trim());
+            sqlcm.Parameters.AddWithValue("@Soluong", txtSoLuong.Text.Trim());
+            int check = sqlcm.ExecuteNonQuery();
+            if (check > 0)
+            {
+                MessageBox.Show("Nhập thành công");
+                conn.KhoiTao(dgv_Hanghoa, @"select * from tblHangHoa");
+                txtMaHH.Text = txtTenHH.Text = txtMaNCC.Text = txtSoLuong.Text = string.Empty;
+            }
+            else
+            {
+                MessageBox.Show("Có lỗi");
+            }
+            conn.DongKetNoi();
+        }
+
      
     }
 }

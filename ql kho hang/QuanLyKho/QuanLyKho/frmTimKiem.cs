4249d3d1abd12a797197f2cb9c25daca2b2e3bc3
windatm <tuanphongmta0205@gmail.com>
2017-10-16 16:07:32 +0700

    FrmTimKiem2

--- ql kho hang/QuanLyKho/QuanLyKho/frmTimKiem.cs
+++ ql kho hang/QuanLyKho/QuanLyKho/frmTimKiem.cs
@@ -41,5 +41,50 @@ namespace QuanLyKho
             conn.DongKetNoi();
         }
 
+        private void butTimTen_Click(object sender, EventArgs e)
+        {
+            conn.MoKetNoi();
+            SqlCommand sqlcm = new SqlCommand("TimTenHH", conn.conn);
+            sqlcm.CommandType = CommandType.StoredProcedure;
+            sqlcm.Parameters.Add("@TenHH", txtTenHH.Text);
+            SqlDataAdapter da = new SqlDataAdapter(sqlcm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DataView dv = new DataView(dt);
+            dataGridView1.DataSource = dv;
+            txtTenHH.Text = string.Empty;
+            conn.DongKetNoi();
+        }
+
+        private void butTimNCC_Click(object sender, EventArgs e)
+        {
+            conn.MoKetNoi();
+            SqlCommand sqlcm = new SqlCommand("TimMaNCC", conn.conn);
+            sqlcm.CommandType = CommandType.StoredProcedure;
+            sqlcm.Parameters.Add("@MaNCC", txtMaNCC.Text);
+            SqlDataAdapter da = new SqlDataAdapter(sqlcm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DataView dv = new DataView(dt);
+            dataGridView1.DataSource = dv;
+            txtMaNCC.Text = string.Empty;
+            conn.DongKetNoi();
+        }
+
+        private void butTimSL_Click(object sender, EventArgs e)
+        {
+            conn.MoKetNoi();
+            SqlCommand sqlcm = new SqlCommand("TimSL", conn.conn);
+            sqlcm.CommandType = CommandType.StoredProcedure;
+            sqlcm.Parameters.Add("@SoLuong", txtSoLuong.Text);
+            SqlDataAdapter da = new SqlDataAdapter(sqlcm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DataView dv = new DataView(dt);
+            dataGridView1.DataSource = dv;
+            txtSoLuong.Text = string.Empty;
+            conn.DongKetNoi();
+        }
+
     }
 }

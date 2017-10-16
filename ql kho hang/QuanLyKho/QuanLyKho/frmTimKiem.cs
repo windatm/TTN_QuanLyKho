3618e31001b4d669e47a140e80296c127dd1315b
windatm <tuanphongmta0205@gmail.com>
2017-10-16 15:03:47 +0700

    FrmTimKiem1

--- ql kho hang/QuanLyKho/QuanLyKho/frmTimKiem.cs
+++ ql kho hang/QuanLyKho/QuanLyKho/frmTimKiem.cs
@@ -18,6 +18,28 @@ namespace QuanLyKho
             InitializeComponent();
         }
 
-     
+        ConnectData conn = new ConnectData();
+        public string constr = @"select * from tblHangHoa";
+
+        private void frmNhapHang_Load(object sender, EventArgs e)
+        {
+            conn.KhoiTao(dataGridView1, constr);
+        }
+
+        private void butTimMa_Click(object sender, EventArgs e)
+        {
+            conn.MoKetNoi();
+            SqlCommand sqlcm = new SqlCommand("TimMaHH", conn.conn);
+            sqlcm.CommandType = CommandType.StoredProcedure;
+            sqlcm.Parameters.Add("@MaHH", txtMaHH.Text);
+            SqlDataAdapter da = new SqlDataAdapter(sqlcm);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DataView dv = new DataView(dt);
+            dataGridView1.DataSource = dv;
+            txtMaHH.Text = string.Empty;
+            conn.DongKetNoi();
+        }
+
     }
 }

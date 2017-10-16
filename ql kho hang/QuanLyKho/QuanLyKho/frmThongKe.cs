e2e814f4b9e62ed1b454e3e56134b185e915f752
taikhoana1 <noidovatoi11196@gmail.com>
2017-10-16 14:07:24 +0700

    form tk l1

--- ql kho hang/QuanLyKho/QuanLyKho/frmThongKe.cs
+++ ql kho hang/QuanLyKho/QuanLyKho/frmThongKe.cs
@@ -17,6 +17,34 @@ namespace QuanLyKho
         {
             InitializeComponent();
         }
-       
+        ConnectData con = new ConnectData();
+        public string constr = @"Data Source=HP-PC;Initial Catalog=Project_4;Integrated Security=True";
+
+        private void btn_nhap_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(constr);
+            SqlCommand com = new SqlCommand();
+            com.CommandText = "ThongKeNhap";
+            com.CommandType = CommandType.StoredProcedure;
+            com.Connection = cn;
+            SqlDataAdapter adap = new SqlDataAdapter(com);
+            DataTable dt = new DataTable();
+            adap.Fill(dt);
+            if (dt.Rows.Count > 0)
+            {
+                dgv_thongke.DataSource = dt;
+            }
+            else
+            {
+                MessageBox.Show("Data is empty!");
+            }
+            con.DongKetNoi();
+        }
+
+        private void frmThongKe_Load(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
